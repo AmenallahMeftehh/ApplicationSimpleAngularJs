87ac7b2554f18b537d8049ffc13f50059d86e6f8
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-11 17:14:13 +0100

    Suite directives

--- Directives/musician.js
+++ Directives/musician.js
@@ -1,7 +1,11 @@
 angular.module("app").directive("musician",function(){
 return{
   restrict: 'E',
-  templateUrl:'templates/musician.html'
+  templateUrl:'templates/musician.html',
+  replace : true,
+  scope:{
+    artist : '=artist'
+  }
 }
 
 })
